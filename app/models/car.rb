04186d61b966434069ae89eb239d81832d01b254
Santiago Ruiz <dolarsrg@gmail.com>
2024-11-02 20:15:23 +0100

    feat: matrículas en mayúsculas y sin espacios dobles

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,4 +1,11 @@
 class Car < ApplicationRecord
   has_one_attached :photo
   validates :plate, presence: true, uniqueness: true
+
+  before_validation :upcase_plate
+
+  def upcase_plate
+    # Elimina espacios y convierte a mayúsculas
+    self.plate = plate.strip.upcase
+  end
 end
