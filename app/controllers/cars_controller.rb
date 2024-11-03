6d21d61b8082d54002e4875cb1957be10a3088aa
Javier N. Pérez <javier.perez@chappsolutions.com>
2024-11-03 20:05:44 +0100

    Add search method and update template

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,6 +1,10 @@
 class CarsController < ApplicationController
   def index
-    @cars = Car.order(:plate).all
+    if params[:search].present?
+      @cars = Car.where("plate LIKE ?", "%#{params[:search]}%").order(:plate)
+    else
+      @cars = Car.order(:plate).all
+    end
   end
 
   def new
